49234498f49398a50186f413fbdee73f3a10d3de
reroche <rroche@andrew.cmu.edu>
2019-06-04 21:53:59 -0400

    general settings

--- config.h
+++ config.h
@@ -8,10 +8,10 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	/*                 fg         bg       */
+	[SchemeNorm] = { "#c0c5ce", "#1b1b22" },
+	[SchemeSel] = { "#1b1b22", "#ebcb8b" },
+	[SchemeOut] = { "#ebcb8b", "#1b1b22" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
